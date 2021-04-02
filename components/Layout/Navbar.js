9423a3f45b02fc73a526b734d6f62acd7aef93b7
yuweichen1008 <yuweiichen@gmail.com>
2021-04-02 23:15:44 +0800

    add auth with mongo p1

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -1,6 +1,16 @@
 import Link from "next/link"
 
 const Navbar = () => {
+
+    const [, { mutate }] = useUser();
+    const handleLogout = async () => {
+        await fetch('/api/auth', {
+          method: 'DELETE',
+        });
+        // set the user state to null
+        mutate(null);
+    };
+    
     return (
         <nav className="flex justify-between ">
             <p>My App</p>
@@ -8,7 +18,7 @@ const Navbar = () => {
                 <Link href='/'><a>家目錄</a></Link> |
                 <Link href='/about'><a>關於我</a></Link> |
                 <Link href='/contact'><a>與我聯絡</a></Link>
-                <Link href='/logout'>Logout</Link>
+                <button onClick={handleLogout}>Logout</button>
             </div>
         </nav>
     );
--- pages/api/user.js
+++ pages/api/user.js
@@ -1,14 +1,43 @@
-import { withIronSession } from "next-iron-session";
+import nextConnect from 'next-connect';
+import isEmail from 'validator/lib/isEmail';
+import normalizeEmail from 'validator/lib/normalizeEmail';
+import bcrypt from 'bcryptjs';
+import middleware from '../../middlewares/middleware';
+import { extractUser } from '../../lib/api-helpers';
 
-function handler(req, res, session) {
-  const user = req.session.get("user");
-  res.send({ user });
-}
+const handler = nextConnect();
+handler.use(middleware);
 
-export default withIronSession(handler, {
-  password: "complex_password_at_least_32_characters_long",
-  // if your localhost is served on http:// then disable the secure flag
-  cookieOptions: {
-    secure: process.env.NODE_ENV === "production",
-  },
+// POST /api/users
+handler.post(async (req, res) => {
+  const { name, password } = req.body;
+  const email = normalizeEmail(req.body.email); // this is to handle things like jane.doe@gmail.com and janedoe@gmail.com being the same
+  if (!isEmail(email)) {
+    res.status(400).send('The email you entered is invalid.');
+    return;
+  }
+  if (!password || !name) {
+    res.status(400).send('Missing field(s)');
+    return;
+  }
+  // check if email existed
+  if ((await req.db.collection('users').countDocuments({ email })) > 0) {
+    res.status(403).send('The email has already been used.');
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await req.db
+    .collection('users')
+    .insertOne({ email, password: hashedPassword, name })
+    .then(({ ops }) => ops[0]);
+  req.logIn(user, (err) => {
+    if (err) throw err;
+    // when we finally log in, return the (filtered) user object
+    res.status(201).json({
+      user: extractUser(req),
+    });
+  });
 });
+
+// handler.get(async (req, res) => res.json({ user: extractUser(req) }));
+
+export default handler;
